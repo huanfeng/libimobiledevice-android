1d79fcee196cba1cdecac885bc65af98973cf007
lunzic <lunzic@gmail.com>
2014-04-04 22:33:13 +0800

    compiled usbmuxd succeed

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -6,14 +6,15 @@ include $(LOCAL_PATH)/libplist.mk
 include $(LOCAL_PATH)/libusbmuxd.mk
 
 include $(LOCAL_PATH)/libusbx.mk
-#include $(LOCAL_PATH)/usbmuxd.mk
 
 include $(LOCAL_PATH)/openssl.mk
 
 include $(LOCAL_PATH)/libimobiledevice.mk
 
-#include $(LOCAL_PATH)/libzip.mk
-#include $(LOCAL_PATH)/ideviceinstaller.mk
+include $(LOCAL_PATH)/usbmuxd.mk
+
+include $(LOCAL_PATH)/libzip.mk
+include $(LOCAL_PATH)/ideviceinstaller.mk
 
 #include $(LOCAL_PATH)/libfuse.mk
 #include $(LOCAL_PATH)/ifuse.mk
