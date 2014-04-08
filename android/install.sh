763438b6a1d5146c369f418af13a830e727bd6b1
lunzic <lunzic@gmail.com>
2014-04-08 10:23:59 +0800

    usbmuxd daemon run succeed

--- android/install.sh
+++ android/install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 PATH_LOCAL_LIB="libs/armeabi-v7a"
-PATH_SYSTEM_LIB="/system/lib"
-PATH_SYSTEM_BIN="/data/local/tmp"
+PATH_DEVICE_LIB="/data/local/tmp"
 
 # Make the system partition writable
-adb shell su -c "mount -o remount,rw /system"
+#adb shell su -c "mount -o remount,rw /system"
 
 # Install so
 echo "----------Install so--------------"
 for S in libiconv.so libxml2.so libplist.so libplist++.so libusb.so libusbmuxd.so libcrypto.so libssl.so libimobiledevice.so libzip.so
 do
-  adb shell rm "$PATH_SYSTEM_LIB/$S"
-  adb push "$PATH_LOCAL_LIB/$S" $PATH_SYSTEM_LIB
+  adb shell rm "$PATH_DEVICE_LIB/$S"
+  adb push "$PATH_LOCAL_LIB/$S" $PATH_DEVICE_LIB
 done
 
 
@@ -20,22 +19,22 @@ done
 echo "----------Install binary--------------"
 for B in listdevs openssl usbmuxdd ideviceid ideviceinfo idevicedate idevicediagnostics idevicescreenshot idevicesyslog ideviceinstaller ifuse fusermount
 do
-  adb shell rm "$PATH_SYSTEM_BIN/$B"
-  adb push "$PATH_LOCAL_LIB/$B" $PATH_SYSTEM_BIN
-  adb shell su -c "chmod 0755 $PATH_SYSTEM_BIN/$B"
+  adb shell rm "$PATH_DEVICE_LIB/$B"
+  adb push "$PATH_LOCAL_LIB/$B" $PATH_DEVICE_LIB
+  adb shell su -c "chmod 0755 $PATH_DEVICE_LIB/$B"
 done
 
 # Make the system partition read only again
-adb shell su -c "mount -o remount,ro /system"
+#adb shell su -c "mount -o remount,ro /system"
 
 # Run listdevs to
-#adb shell su -c "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$PATH_DEVICE"
-#adb shell su -c "export PATH=\$PATH:$PATH_DEVICE"
+adb shell su -c "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PATH_DEVICE_LIB"
+adb shell su -c "export PATH=$PATH:$PATH_DEVICE_LIB"
 
 echo "----------list usb devs--------------"
-adb shell su -c "$PATH_SYSTEM_BIN/listdevs"
-adb shell su -c "$PATH_SYSTEM_BIN/usbmuxdd"
-adb shell su -c "$PATH_SYSTEM_BIN/ideviceid -l"
+adb shell su -c "$PATH_DEVICE_LIB/listdevs"
+adb shell su -c "$PATH_DEVICE_LIB/usbmuxdd"
+adb shell su -c "$PATH_DEVICE_LIB/ideviceid -l"
 
-echo "----------ls $PATH_SYSTEM_BIN--------------"
-adb shell su -c "ls $PATH_SYSTEM_BIN"
+echo "----------ls $PATH_DEVICE_LIB--------------"
+adb shell su -c "ls $PATH_DEVICE_LIB"
