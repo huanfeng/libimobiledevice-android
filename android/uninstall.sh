5a3cdda908f53ee3ebc5fec30ba8a88fce2cd06a
lunzii <lunzic@gmail.com>
2014-04-08 20:40:44 +0800

    release libimobiledevice-android 1.1.5 for iOS 7.1

--- android/uninstall.sh
+++ android/uninstall.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 PATH_LOCAL_LIB="libs/armeabi-v7a"
-PATH_SYSTEM_LIB="/system/lib"
-PATH_SYSTEM_BIN="/data/local/tmp"
+PATH_DEVICE_LIB="/data/local/tmp"
 
 # Make the system partition writable
 #adb shell su -c "mount -o remount,rw /system"
@@ -11,7 +10,7 @@ PATH_SYSTEM_BIN="/data/local/tmp"
 echo "----------Uninstall so--------------"
 for S in libiconv.so libxml2.so libplist.so libplist++.so libusb.so libusbmuxd.so libcrypto.so libssl.so libimobiledevice.so libzip.so
 do
-  adb shell rm "$PATH_SYSTEM_LIB/$S"
+  adb shell rm "$PATH_DEVICE_LIB/$S"
 done
 
 
@@ -19,15 +18,15 @@ done
 echo "----------Uninstall binary--------------"
 for B in listdevs openssl usbmuxdd ideviceid ideviceinfo idevicedate idevicediagnostics idevicescreenshot idevicesyslog ideviceinstaller ifuse fusermount
 do
-  adb shell rm "$PATH_SYSTEM_BIN/$B"
+  adb shell rm "$PATH_DEVICE_LIB/$B"
 done
 
 # Make the system partition read only again
 #adb shell su -c "mount -o remount,ro /system"
 
 
-echo "----------ls $PATH_SYSTEM_BIN--------------"
-adb shell su -c "ls $PATH_SYSTEM_BIN"
+echo "----------ls $PATH_DEVICE_LIB--------------"
+adb shell su -c "ls $PATH_DEVICE_LIB"
 
 echo "----------list usb devs--------------"
 adb shell su -c "listdevs"
